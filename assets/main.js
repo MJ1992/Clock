c3dc8b35f86af5c8a9dc0e9f33db60a2b1e31780
MJ <jangid.manish40@gmail.com>
2017-08-30 01:32:42 +0530

    Clock 1.0.1

--- assets/main.js
+++ assets/main.js
@@ -3,20 +3,20 @@ const minHand = document.querySelector('.hand-minute');
 const hourHand = document.querySelector('.hand-hour');
 
 function setDate(){
-	var now = new Date();
-	var seconds = now.getSeconds();
+	const now = new Date();
+	const seconds = now.getSeconds();
 	const secondsToDegrees = ((seconds/60)*360)-90;
 	console.log(seconds);
 	secondHand.style.transform = `rotate(${secondsToDegrees}deg)`;
 
-	var mins = now.getMinutes();
+	const mins = now.getMinutes();
 	const minsToDegrees = ((mins/60)*360)-90;
 	console.log(mins);
 	minHand.style.transform = `rotate(${minsToDegrees}deg)`;
 
-	var hour = now.getMinutes();
+	const hour = now.getHours();
 	const hoursToDegrees = ((hour/12)*360)-90;
-	console.log(hour);
+	console.log(hoursToDegrees);
 	hourHand.style.transform = `rotate(${hoursToDegrees}deg)`;
 
 	}
